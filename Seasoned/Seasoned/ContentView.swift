ef35a1ae0670745a225d0a7fa6fac4d259f68642
Djie <64975278+Dejie1@users.noreply.github.com>
2024-05-31 14:54:00 +1000

    Test

--- Seasoned/Seasoned/ContentView.swift
+++ Seasoned/Seasoned/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Fingers Crooossed!")
+            Text("Fingers Crooossed in Dev!")
         }
         .padding()
     }
