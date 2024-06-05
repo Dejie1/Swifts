502fbd233177893878f7535bb7773348860c63c3
Srish070707 <srishti.sirohi@student.uts.edu.au>
2024-06-05 22:42:10 +1000

    Updates sessions

--- Seasoned/Seasoned/views/Sessions(S).swift
+++ Seasoned/Seasoned/views/Sessions(S).swift
@@ -7,19 +7,20 @@
 
 import SwiftUI
 
-
-    var body: some View {
-        
-    
-        TabView{
-            ViewA()
-            ViewB()
-            ViewC()
-               
-        }
+struct Sessions_S_: View {
+  var body: some View {
+    NavigationStack {
+      ScrollView {
+        // Your scroll view content
+      }
+      HStack {
+        Text("Chats")
+      }
+      .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading) // Position top left
     }
-
+  }
+}
 
 #Preview {
-    Responses()
+  Sessions_S_()
 }
