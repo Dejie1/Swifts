818e48de4443336bcf2b081574ea20acb799cf28
rryanwalsh <ryan.j.walsh@student.uts.edu.au>
2024-06-05 10:38:42 +1000

    finished MeetUpView

--- Seasoned/Seasoned/MentorMeetUp.swift
+++ Seasoned/Seasoned/MentorMeetUp.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct MentorMeetUp: View {
     @State private var description: String = ""
     @State private var meetUpDate = Date()
+    @State private var selectedCategories: Set<String> = []
     
     var body: some View {
         VStack {
@@ -29,16 +30,77 @@ struct MentorMeetUp: View {
                     .padding(.horizontal)
                     .textFieldStyle(.roundedBorder)
                 
-                Text("Pick a Date and Time")
-                    .font(.title3)
-                    .fontWeight(.semibold)
-                    .padding(.horizontal)
+          
+                
+                VStack(alignment: .leading) {
+                    Text("Pick a Date and Time")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .padding(.horizontal)
+
+                    DatePicker("Meet Up Date", selection: $meetUpDate, in: Date()...)
+                        .padding(.horizontal)
+                    
+                }
+                .padding(.top)
+                .padding(.bottom)
+              
+                VStack{
+
+                    VStack(spacing: 15) {
+                        HStack(spacing: 20) {
+                            CategoryButton(iconName: "dollarsign.circle",
+                                           label: "Finance",
+                                           color: Color(red: 0, green: 0.588, blue: 0.533),
+                                           selectedCategories: $selectedCategories)
+                            
+                            CategoryButton(iconName: "house.fill",
+                                           label: "Housing",
+                                           color: Color(red:0.29804, green:0.68627, blue:0.31373),
+                                           selectedCategories: $selectedCategories)
+                            
+                            CategoryButton(iconName: "cross.circle.fill",
+                                           label: "Health",
+                                           color: Color(red:0.12941, green:0.58824, blue:0.95294),
+                                           selectedCategories: $selectedCategories)
+                        }
+                        
+                        HStack(spacing: 20) {
+                            CategoryButton(iconName: "graduationcap.fill",
+                                           label: "Education",
+                                           color: Color(red:1.00000, green:0.59608, blue:0.00000),
+                                           selectedCategories: $selectedCategories)
+                            
+                            CategoryButton(iconName: "tram.fill",
+                                           label: "Transport",
+                                           color: Color(red:0.95686, green:0.26275, blue:0.21176),
+                                           selectedCategories: $selectedCategories)
+                            
+                            CategoryButton(iconName: "person.3.fill",
+                                           label: "Events",
+                                           color: Color(red:0.61176, green:0.15294, blue:0.69020),
+                                           selectedCategories: $selectedCategories)
+                        }
+                    }
                     .padding(.top)
+                    .padding(.bottom)
                 
-                DatePicker("Meet Up Date", selection: $meetUpDate, in: Date()...)
-                    .padding(.horizontal)
-                
-                
+                    Button(action:{} ){
+                       
+                            Text("Send MeetUp Request")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(selectedCategories.isEmpty ? Color.gray: Color.blue)
+                                .cornerRadius(30)
+                                .padding(.horizontal)
+                        }
+                    
+                    .disabled(selectedCategories.isEmpty)
+                    .padding()
+                }
+
                 
                 
             }
