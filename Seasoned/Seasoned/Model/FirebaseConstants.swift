7d9a14d3955a917d36ad4cd47177604ca4c94f3d
Djie <64975278+Dejie1@users.noreply.github.com>
2024-06-11 13:12:43 +1000

    feat: messaging & refactoring

---
+++ Seasoned/Seasoned/Model/FirebaseConstants.swift
@@ -0,0 +1,22 @@
+//
+//  FirebaseConstants.swift
+//  Seasoned
+//
+//  Created by De Jie Ang on 6/6/2024.
+//
+
+import Foundation
+
+struct FirebaseConstants {
+    static let fromId = "fromId"
+    static let toId = "toId"
+    static let text = "text"
+    static let timestamp = "timestamp"
+    static let email = "email"
+    static let uid = "uid"
+    static let profileImageUrl = "profileImageUrl"
+    static let messages = "messages"
+    static let users = "users"
+    static let name = "name"
+    static let recentMessages = "recent_messages"
+}
--- Seasoned/Seasoned/Model/User.swift
+++ Seasoned/Seasoned/Model/User.swift
@@ -6,32 +6,21 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
-struct User: Identifiable {
-    var id: String { uid }
+struct User: Identifiable, Codable {
+    @DocumentID var id: String?
     let uid: String
     let email: String
     let profileImageUrl: String
     let userType: String
     var selectedCategories: [String]?
-    var hasSelectedCategories: Bool
-    var name: String?
+    var hasSelectedCategories: Bool?
+    var name: String
     var about: String?
     var age: Int?
     var uni: String?
     var course: String?
     
-    init(data: [String: Any]) {
-        self.uid = data["uid"] as? String ?? ""
-        self.email = data["email"] as? String ?? ""
-        self.profileImageUrl = data["profileImageUrl"] as? String ?? ""
-        self.userType = data["userType"] as? String ?? ""
-        self.selectedCategories = data["selectedCategories"] as? [String] ?? []
-        self.hasSelectedCategories = data["hasSelectedCategories"] as? Bool ?? false
-        self.name = data["name"] as? String ?? ""
-        self.about = data["about"] as? String ?? ""
-        self.age = data["age"] as? Int ?? nil
-        self.uni = data["uni"] as? String ?? ""
-        self.course = data["course"] as? String ?? ""
-    }
 }
